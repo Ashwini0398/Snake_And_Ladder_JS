a051fecfbb62737fdac273bfe9b4e18f68d11dfc
Ashwini0398 <ashwinikhonde03@gmail.com>
2021-05-09 22:35:27 +0530

    ADDED: message

---
+++ diceRoll.js
@@ -0,0 +1,11 @@
+let constant = require('./constants')
+
+class DiceRoll{    
+    insert(){
+        let player1 = constant.readline.question("Enter the player name: ");
+        console.log(`Player1 name ${player1}`);
+        console.log(`${player1} position now => ${constant.plr1_pos}`);
+    }
+}
+
+module.exports = DiceRoll;
